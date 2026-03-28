fbd78ce9194f6c37cc635ff6c5a5b05bb1790be0
kaleb <kalebcode25@gmail.com>
2026-03-28 21:08:11 +0300

    Fix ESM imports: add .js extensions to all schema files

--- frontend-web/src/config/index.ts
+++ frontend-web/src/config/index.ts
@@ -1,6 +1,14 @@
-export const URL = {
-    BASE: 'http://localhost:3000',
-    ORPC : "http://localhost:3000/rpc",
-    BETTER_AUTH: "http://localhost:3000/api/auth",
-    IMAGE : "http://localhost:3000"
-}
+const isdev = true 
+    export const URL = (!isdev)? {
+
+        BASE: 'http://localhost:3000',
+        ORPC : "http://localhost:3000/rpc",
+        BETTER_AUTH: "http://localhost:3000/api/auth",
+        IMAGE : "http://localhost:3000"
+    } : {
+
+        BASE: 'https://one.solvesphr.com',
+        ORPC : "https://one.solvesphr.com/rpc",
+        BETTER_AUTH: "https://one.solvesphr.com/api/auth",
+        IMAGE : "https://one.solvesphr.com"
+    }
